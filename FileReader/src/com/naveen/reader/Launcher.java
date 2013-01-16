2eb3ccdec2e18ce4b938c39036c353141f35bc76
Naveen Ravindran <naveen27in@gmail.com>
2013-01-17 02:06:39 +1100

    changed the renaming method to Files.move from .renameTo() due to issues regarding windows file system and performance

--- FileReader/src/com/naveen/reader/Launcher.java
+++ FileReader/src/com/naveen/reader/Launcher.java
@@ -2,9 +2,28 @@ package com.naveen.reader;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Launcher extends Application{
 
 	/**
 	 * Prepares the application for operation by collecting the inputs and
@@ -14,27 +33,93 @@ public class Launcher {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome");
+		ArrayList<Path> filter_results = new ArrayList<>();
+		//launch(args);
+		
 		System.out.println("enter the folder name");
 		Scanner input = new Scanner(System.in);
-		// Path path = Paths.get(input.nextLine());
-		Path path = Paths.get("C:\\Users\\Naveen\\Desktop\\Testing");
-		ReaderOperations rop_obj = new ReaderOperations(path);
+		Path path = Paths.get(input.nextLine());		
 		System.out.println("Enter the extension of the files");
 		String ext = input.nextLine();
+		ReaderOperations rop_obj = new ReaderOperations(path, ext);
 		System.out.println("Enter your criteria for filtering:");
-
 		String criteria = input.nextLine();
-		String criterion[];
-		if (criteria.contains("*")) {
-			criterion = criteria.split("[*]");
-			System.out.println("printing the criterias");
-			for (String criter : criterion) {
-				rop_obj.filter(criter, ext);
+		
+		Pattern wild_pattern = Pattern.compile("[*]");
+		String criter_parts[] = null;
+		if(criteria.contains("*")){
+			criter_parts = wild_pattern.split(criteria);
+			
+			StringBuilder newCriteria = new StringBuilder();		
+			int count = 1;
+			for(String part : criter_parts){
+				newCriteria.append(part);				
+					newCriteria.append(".*");				
+			}			
+			filter_results = rop_obj.filter(newCriteria.toString());
+			System.out.println("printing the filtered results");
+			for(Path file : filter_results){
+				System.out.println(file.toString());
+				
 			}
-		} else{
-			rop_obj.filter(criteria, ext);
+			rop_obj.renameFiles(criter_parts, filter_results);			
 		}
+		else {
+			//filter_results.add(e)
+			System.out.println("Yet to Implement");
+			
+		}		
 		input.close();
 		System.out.println("bye");
+	}
+	
+	@Override
+	public void start(Stage primaryStage){
+		
+		primaryStage.setTitle("This is F I L E R E A D E R");
+		
+		GridPane mygrid = new GridPane();
+		mygrid.setAlignment(Pos.TOP_CENTER);
+		mygrid.setHgap(10);
+		mygrid.setVgap(10);
+		mygrid.setPadding(new Insets(25,25,25,25));
+		
+		Text welcome = new Text("Welcome!!");		
+		welcome.setFont(Font.font("Tahoma", FontWeight.MEDIUM, 20));
+		mygrid.add(welcome, 0, 0,2,1);
+		
+		Label location_label = new Label("Enter the location:");
+		mygrid.add(location_label, 0,1);
+		TextField locationTf = new TextField(); 
+		mygrid.add(locationTf, 1, 1);
+		Label criterion_label = new Label("Enter the Criteria:");
+		mygrid.add(criterion_label, 0,2);
+		TextField criterionTf = new TextField(); 
+		mygrid.add(criterionTf, 1, 2);
+		HBox btn_box = new HBox(10);
+		btn_box.setAlignment(Pos.BOTTOM_RIGHT);
+		Button filterBtn = new Button("Filter");		
+		Button renameBtn = new Button("Rename");
+		btn_box.getChildren().add(filterBtn);
+		filterBtn.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				
+				
+			}
+		});
+		btn_box.getChildren().add(renameBtn);
+		mygrid.add(btn_box, 1, 3);
+		
+		TextArea notify = new TextArea(" ");	
+		notify.setWrapText(true);
+		mygrid.add(notify,0, 5, 2,2);
+		
+
+		Scene scene = new Scene(mygrid, 500, 275);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 	}
 }
--- FileReader/src/com/naveen/reader/ReaderOperations.java
+++ FileReader/src/com/naveen/reader/ReaderOperations.java
@@ -1,18 +1,22 @@
 package com.naveen.reader;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
-
 
 public class ReaderOperations {
 
 	private static Path folder_path;
+	private static String extension;
 
-	public ReaderOperations(Path f_path) {
+	public ReaderOperations(Path f_path, String file_ext) {
 		folder_path = f_path;
+		extension = file_ext;
 	}
 
 	public void listFiles() {
@@ -20,38 +24,67 @@ public class ReaderOperations {
 	}
 
 	/**
-	 * this method filters the files in the folder according to the critereia of
-	 * the user
+	 * filters the files according to the criteria and the extension
 	 * 
 	 * @param criteria
+	 * @param criter_parts
+	 * @param ext
+	 * @return results
 	 */
-	public void filter(String criteria, String ext) {		
-		System.out.println("ext:"+ext + "crit" + criteria);
-		
-		Pattern myPattern = Pattern.compile(criteria);
-		
-		
+	public ArrayList<Path> filter(String criteria) {
+		System.out.println(criteria);
+		ArrayList<Path> results = new ArrayList<>();
 		System.out.println("filteringgggg....." + folder_path);
-		
 		try (DirectoryStream<Path> search_dir = Files
 				.newDirectoryStream(folder_path);) {
-			
-			for (Path path : search_dir) {			
-				
-				if(path.toFile().getName().endsWith(ext)){
-					System.out.println("before split path :"+path);					
-					String[] name_parts = myPattern.split(path.toString());
-					System.out.println("after split with criteria :"+criteria);
-					StringBuilder new_name = new StringBuilder();					
-					for(String s : name_parts){						
-						new_name.append(s);
-					}					
-					System.out.println(new_name);
-					path.toFile().renameTo(new File(new_name.toString()));
+			String fileName;
+			for (Path path : search_dir) {
+				fileName = path.getFileName().toString();
+				// checking for extension and overall criteria
+				if (fileName.endsWith(extension)
+						&& Pattern.matches(criteria, fileName)) {
+					results.add(path);
 				}
 			}
+
 		} catch (Exception e) {
-             e.printStackTrace();
+			e.printStackTrace();
+		}
+		return results;
+	}
+
+	/**
+	 * renames all the files in the filtered paths
+	 * 
+	 * @param criter_parts
+	 * @param filteredPaths
+	 */
+	public void renameFiles(String[] criter_parts, ArrayList<Path> filteredPaths) {
+
+		for (Path filePath : filteredPaths) {
+			System.out.println("renaming for ::::" + filePath);
+			for (String criterion : criter_parts) {
+				if (!criterion.contentEquals(extension)) {
+					Pattern myPattern = Pattern.compile(criterion);
+					String parts[] = myPattern.split(filePath.getFileName()
+							.toString());
+					StringBuilder rebuilder = new StringBuilder();
+					for (String part : parts) {
+						rebuilder.append(part);
+					}
+					try {
+						System.out.println("renaming for criter: " + criterion);
+						filePath = Files.move(filePath,
+								filePath.resolveSibling(rebuilder.toString()));
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				} else {
+					System.out
+							.println("yet to implement for criteria same as extension");
+				}
+			}
+			System.out.println("final modified name:::" + filePath);
 		}
 	}
 }
